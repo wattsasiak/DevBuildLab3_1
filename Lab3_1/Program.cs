4be797724be91fdddfd3199313c6630f27d241df
Asia Watts <AsiaWatts@quickenloans.com>
2021-07-26 15:40:38 -0400

    First commit

---
+++ Lab3_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lab 3.1
+            //Provide information about students in a class
+            //Prompt the user to ask about a particular student
+            //Give proper responses according to user-submitted information
+            //Ask if user would like to learn about another student
+
+            string[] studentNames = { "Asia", "John", "Sue" };
+            string[] favFood = { "Pizza", "Tacos", "Salad" };
+            string[] prevTitle = { "Underwriter", "Astronaut", "CEO" };
+           
+
+
+        }
+    }
+}
